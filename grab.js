a25edab3e998b05fdc1ae6b2a0c657f9f3206c74
Jtkantor198 <Jtkantor198@gmail.com>
2016-12-23 23:15:17 -0500

    Adds Recursive
    -Adds recursive grabbing and moving
    -Removes browserify-string dependency by using steams to interface with
    browserify's api directly

--- grab.js
+++ grab.js
@@ -1,85 +1,131 @@
 var Finder = require('fs-finder');
 var fse = require('fs-extra');
 var fs = require('fs');
+var path = require('path');
 module.exports = function(regex){
-    if (typeof regex == "string"){
-        filename = regex.substring(regex.lastIndexOf("/")+1,regex.length);
-        pathname = regex.substring(0,regex.lastIndexOf("/"));
-        var files = Finder.in(__dirname + pathname).findFiles(filename);
-    }
-    else if (regex.exclude){
-        filename = regex.find.substring(regex.find.lastIndexOf("/")+1,regex.find.length);
-        pathname = regex.find.substring(0,regex.find.lastIndexOf("/"));
-        var files = Finder.in(__dirname + pathname).exclude(regex.exclude).findFiles(filename);
-    }
-    for (var i in files){
-        name = files[i];
-        files[i] = {}
-        files[i].data = fs.readFileSync(name);
-        name = name.substring((name.lastIndexOf("\\")+1 || name.lastIndexOf("/")+1), name.length);
-        files[i].name = name;
-    }
-    //Read files keep names
-    this.current = files;
-    this.transform = function(transformer){
-        /*this.current = this.current.map(function(item){
-            item.data = transformer(item.data);
-            return item;
-        })*/
-        //Apply transformer
-        //Check if promise is returned, if so async chain down to file write
-        for (var i=0;i<this.current.length;i++){
-            if (this.current[i].data.then){
-                if (!this.current[i].stack){
-                    this.current[i].stack = [];
-                }
-                if (this.current[i].stack.length == 0 && !this.current[i].waiting){
-                    var current = this.current[i];
-                    function delayExecution(value){
-                        //Excute until stack empty
-                         current.data = transformer(value);
-                         while (current.stack.length > 0 && !promise){
-                             if(current.data.then){
-                                 var promise = true;
-                                 current.then(delayExecution);
-                             }
-                             else{
-                                 current.data = current.stack[0].call(this, current.data);
-                                 current.stack.splice(0,1);
-                             }
-                         }
-                    }
-                    this.current[i].data.then(delayExecution);
-                    current.waiting = true;
+        if (typeof regex == "string"){
+            filename = regex.substring(regex.lastIndexOf("/")+1,regex.length);
+            pathname = regex.substring(0,regex.lastIndexOf("/"));
+            var files = Finder.in(pathname).findFiles(filename);
+        }
+        else if (regex.exclude || regex.recursive){
+            filename = regex.find.substring(regex.find.lastIndexOf("/")+1,regex.find.length);
+            pathname = regex.find.substring(0,regex.find.lastIndexOf("/"));
+            if (regex.exclude){
+                if (regex.recursive){
+                    var files = Finder.from(pathname).exclude(regex.exclude).findFiles(filename);
                 }
                 else{
-                    this.current[i].stack.push(transformer);
+                    var files = Finder.in(pathname).exclude(regex.exclude).findFiles(filename);
                 }
             }
             else{
-                //apply transformer
-                //console.log(this.current[i].name);
-                this.current[i].data = transformer(this.current[i].data);
+                if (regex.recursive){
+                    var files = Finder.from(pathname).findFiles(filename);
+                }
+                else{
+                    var files = Finder.in(pathname).findFiles(filename);
+                }
+            }
+        }
+        for (var i in files){
+            name = files[i];
+            files[i] = {};
+            files[i].data = fs.readFileSync(name);
+            pathname = path.resolve(process.cwd().replace(/\\+/g,"/").replace(/\/+/g,"/"), pathname.replace(/\\+/g,"/").replace(/\/+/g,"/"));
+            name = name.replace(/\\+/g,"/").replace(/\/+/g,"/");
+            files[i].path = name.substring(name.lastIndexOf(pathname)+pathname.length+1, name.length).replace(/\\+/g,"/").replace(/\/+/g,"/");
+            name = name.substring((name.lastIndexOf("\\")+1 || name.lastIndexOf("/")+1), name.length);
+            files[i].name = name;
+        }
+        //Read files keep names
+        this.current = files;
+        this.transform = function(transformer){
+            /*this.current = this.current.map(function(item){
+                item.data = transformer(item.data);
+                return item;
+            })*/
+            //Apply transformer
+            //Check if promise is returned, if so async chain down to file write
+            for (var i=0;i<this.current.length;i++){
+                if (this.current[i].data.then){
+                    if (!this.current[i].stack){
+                        this.current[i].stack = [];
+                    }
+                    if (this.current[i].stack.length == 0 && !this.current[i].waiting){
+                        function delayExecution(value){
+                            var current = this.current[i];
+                            //Excute until stack empty
+                             current.data = transformer(value);
+                             while (current.stack.length > 0 && !promise){
+                                 if(current.data.then){
+                                     var promise = true;
+                                     current.then(delayExecution);
+                                 }
+                                 else{
+                                     current.data = current.stack[0].call(this, current.data);
+                                     current.stack.splice(0,1);
+                                 }
+                             }
+                        }
+                        this.current[i].data.then(delayExecution);
+                        current.waiting = true;
+                    }
+                    else{
+                        this.current[i].stack.push(transformer);
+                    }
+                }
+                else{
+                    //apply transformer
+                    //console.log(this.current[i].name);
+                    this.current[i].data = transformer(this.current[i].data);
+                }
+            }
+            return this;
+        }
+        this.moveTo = function(dir){
+            if (dir.recursive){
+                dir.path = dir.path.replace(/\\/g,"/");
+                this.current = this.current.map(function(item){
+                    if (item.data.then){
+                        //console.log("out: "+(dir.path + "/"+ item.path).replace(/\\+/g,"/").replace(/\/+/g,"/"));
+                        if (!item.waiting && (!item.stack || item.stack.length == 0)){
+                            item.data.then(function(value){fse.outputFileSync((dir.path + "/"+ item.path).replace(/\\+/g,"/").replace(/\/+/g,"/"), value);});
+                        }
+                        else{
+                            item.stack.push(function(input){fse.outputFileSync((dir.path + "/"+ item.path).replace(/\\+/g,"/").replace(/\/+/g,"/"), item.data)});
+                        }
+                    }
+                    else{
+                        fse.outputFileSync((dir.path + "/"+ item.path).replace(/\\+/g,"/").replace(/\/+/g,"/"), item.data);
+                    }
+                });
+                return true;
+            }
+            else{
+                this.current = this.current.map(function(item){
+                    if (item.data.then){
+                        if (!item.waiting && (!item.stack || item.stack.length == 0)){
+                            item.data.then(function(value){fse.outputFileSync(dir + "/"+ item.name, value)});
+                        }
+                        else{
+                            item.stack.push(function(input){fse.outputFileSync(dir + "/"+ item.name, item.data)});
+                        }
+                    }
+                    else{
+                        fse.outputFileSync(dir + "/"+ item.name, item.data);
+                    }
+                });
+                return true;
             }
         }
         return this;
-    }
-    this.moveTo = function(dir){
-        this.current = this.current.map(function(item){
-            if (item.data.then){
-                if (!item.waiting && (!item.stack || item.stack.length == 0)){
-                    item.data.then(function(value){fse.outputFileSync(dir + "/"+ item.name, value)});
-                }
-                else{
-                    item.stack.push(function(input){fse.outputFileSync(dir + "/"+ item.name, item.data)});
-                }
-            }
-            else{
-                fse.outputFileSync(dir + "/"+ item.name, item.data);
-            //If err, create folder
-            }
+};
+module.exports.watch = function(path, repeat){
+    repeat();
+    if (process.argv[2] == "watch"){
+        fs.watch(path, {recursive: true}, (eventType, filename) => {
+            repeat();
         });
-        return true;
     }
-    return this;
-}
+};
--- grabLibs.js
+++ grabLibs.js
@@ -1,25 +1,40 @@
 var UglifyJS = require("uglify-js");
 var CleanCSS = require('clean-css');
-var BrowserifyFn = require('browserify-string');
+var browserify = require('browserify');
+var stream = require('stream');
 module.exports = {
     browserify: function(input, options){
         return new Promise(function(resolve, reject){
-            js = BrowserifyFn(input);
-            js.bundle(function(err, data){
-            	if (err){
-                    throw err;
-            	}
-                output += data;
-                //resolve(output);
-                resolve(output);
-            });
+            try {
+                var code = new stream.Readable();
+                code._read = function(){};
+                code.push(input);
+                code.push(null);
+                var b = browserify();
+                b.add(code);
+                var result = '';
+                var outputStream = new stream.Writable();
+                outputStream._write = function(){return true;};
+                outputStream.write = function(data){
+                    result+=data;
+                    return true;
+                };
+                outputStream.end = function(){
+                    resolve(result);
+                    return true;
+                };
+                b.bundle().pipe(outputStream);
+            }
+            catch (e){
+                console.log(e);
+            }
         });
     },
     uglify: function(input, options){
-        stuff = UglifyJS.parse(input);
-        stuff.figure_out_scope();
-        stuff =  stuff.transform(UglifyJS.Compressor()).print_to_string();
-        return stuff;
+        uglyStuff = UglifyJS.parse(input);
+        uglyStuff.figure_out_scope();
+        uglyStuff =  uglyStuff.transform(UglifyJS.Compressor()).print_to_string();
+        return uglyStuff;
     },
     minifycss: function(input){
         return new CleanCSS().minify(input).styles;
